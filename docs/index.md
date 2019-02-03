e143dfb1277af5a8802953287014dba20e850be7
Pointy Knife <aaron.cottle@education.nsw.gov.au>
2019-02-03 18:18:04 +1100

    Fixed github thinking it docs was jekyll

--- docs/index.md
+++
@@ -1,2 +0,0 @@
-# Game!
-I want to make a game
--- jekyll/build-site.sh
+++ jekyll/build-site.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env bash
 
+cd "$(dirname "$0")"
 JEKYLL_GITHUB_TOKEN=`cat token.txt` jekyll build -d ../docs/
+rm -f ../docs/*.md ../docs/Gemfile ../docs/Gemfile.lock
